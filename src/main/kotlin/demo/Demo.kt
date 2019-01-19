a3e7c66a28646cb8e36c7afae6a0d439a2b02eb2
Ahmed Rizwan <ahmed@snappymob.com>
2019-01-19 10:00:00 +0500

    Reset starting point

--- src/main/kotlin/demo/Demo.kt
+++ src/main/kotlin/demo/Demo.kt
@@ -2,14 +2,10 @@ package demo
 
 fun main(args: Array<String>) {
     generateRawUsers()
-            .map { rawUser ->
-                val person = fullNameToPerson(rawUser.fullName)
-                println(person)
-
-                val phoneNumber = phoneToDomainPhoneNumber(rawUser.phone)
-                println(phoneNumber)
-
-                // create domainUser
-            }
-
+            .map(::transform)
 }
+
+fun transform(rawUser: RawUser): DomainUser {
+    throw NotImplementedError()
+}
+
